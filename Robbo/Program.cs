aceb019715d811c0b4d08d67c522e82e2bc3026c
Ducas Francis <ducas@ducasfrancis.com>
2012-06-22 14:54:49 +1000

    added vacuum bot implementation

--- Robbo/Program.cs
+++ Robbo/Program.cs
@@ -13,18 +13,17 @@ namespace Robbo
                 PWM.Pin.PWM6, (Cpu.Pin)FEZ_Pin.Digital.Di5, (Cpu.Pin)FEZ_Pin.Digital.Di4,
                 (Cpu.Pin)FEZ_Pin.Digital.Di7
                 );
+            var ultrasonicDistanceSensor = new UltrasonicDistanceSensor(AnalogIn.Pin.Ain5, (Cpu.Pin)FEZ_Pin.Digital.Di13);
             var bot =
-                new DiscoveryBot(
-                    driver,
-                    new UltrasonicDistanceSensor(AnalogIn.Pin.Ain5, (Cpu.Pin)FEZ_Pin.Digital.Di13)
-                    );
                 //new MotorDriverTestBot(driver);
-                //new UltrasonicDistanceSensorTestBot(new UltrasonicDistanceSensor(AnalogIn.Pin.Ain5, (Cpu.Pin)FEZ_Pin.Digital.Di13));
+                //new UltrasonicDistanceSensorTestBot(ultrasonicDistanceSensor);
                 //new MotorTestBot(
                 //    new Motor(PWM.Pin.PWM1, (Cpu.Pin) FEZ_Pin.Digital.Di9, (Cpu.Pin) FEZ_Pin.Digital.Di8),
                 //    new Motor(PWM.Pin.PWM6, (Cpu.Pin) FEZ_Pin.Digital.Di5, (Cpu.Pin) FEZ_Pin.Digital.Di4),
                 //    new OutputPort((Cpu.Pin) FEZ_Pin.Digital.Di7, false)
                 //    );
+                //new DiscoveryBot(driver, ultrasonicDistanceSensor);
+                new VacuumBot(driver, ultrasonicDistanceSensor);
             bot.Go();
         }
     }
---
+++ Robbo/VacuumBot.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Threading;
+using Microsoft.SPOT;
+
+namespace Robbo
+{
+    public class VacuumBot : IDisposable
+    {
+        private const int interruptDistance = 30;
+        private const int fullSpeed = 100;
+        private const int forwardDuration = 500;
+        private const int stopDuration = 500;
+        private const int turnSpeed = 50;
+        private const int maxTurnDuration = 3000;
+
+        private readonly MotorDriver driver;
+        private readonly UltrasonicDistanceSensor front;
+        private readonly Random random = new Random();
+
+        public VacuumBot(MotorDriver driver, UltrasonicDistanceSensor front)
+        {
+            this.driver = driver;
+            this.front = front;
+        }
+
+        public void Go()
+        {
+            var discoverCount = 0;
+            var left = 0;
+            var right = 100;
+
+            driver.Forward(fullSpeed);
+            while (true)
+            {
+                if (front.Distance < interruptDistance)
+                {
+                    driver.Stop();
+                    Thread.Sleep(stopDuration);
+
+                    while (front.Distance < interruptDistance)
+                    {
+                        driver.TurnLeft(turnSpeed);
+                        Thread.Sleep((int)(random.NextDouble() * maxTurnDuration));
+                    }
+
+                    driver.Forward(fullSpeed);
+                    discoverCount = random.Next(10);
+                    left = 0;
+                    continue;
+                }
+
+                if (discoverCount > 0)
+                {
+                    Thread.Sleep(forwardDuration);
+                    discoverCount--;
+                    continue;
+                }
+
+                driver.PwmA = (left < 100 ? ++left : left);
+                driver.PwmB = right;
+
+                Thread.Sleep(500);
+            }
+            // ReSharper disable FunctionNeverReturns
+        }
+        // ReSharper restore FunctionNeverReturns
+
+        public void Dispose()
+        {
+            driver.Dispose();
+            front.Dispose();
+        }
+    }
+}
