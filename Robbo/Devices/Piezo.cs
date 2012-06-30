00ff93636285bc036afcfaea6d1a244a37c976b3
Ducas Francis <ducas@ducasfrancis.com>
2012-06-30 10:08:09 +1000

    Added xml doc to piezo

--- Robbo/Devices/Piezo.cs
+++ Robbo/Devices/Piezo.cs
@@ -4,15 +4,27 @@ using GHIElectronics.NETMF.Hardware;
 
 namespace Robbo.Devices
 {
+    /// <summary>
+    /// Represents a Piezo buzzer.
+    /// </summary>
     public class Piezo : IDisposable
     {
         private readonly PWM pwm;
 
+        /// <summary>
+        /// Creates an instance of the Piezo buzzer.
+        /// </summary>
+        /// <param name="pin">The PWM pin connected to the buzzer.</param>
         public Piezo(PWM.Pin pin)
         {
             pwm = new PWM(pin);
         }
 
+        /// <summary>
+        /// Plays a tone.
+        /// </summary>
+        /// <param name="freq">The desired frequency.</param>
+        /// <param name="duration">The duration of the tone.</param>
         public void Play(int freq, int duration)
         {
             pwm.Set(freq, 50);
@@ -20,11 +32,19 @@ namespace Robbo.Devices
             pwm.Set(freq, 0);
         }
 
+        /// <summary>
+        /// Plays a Tone
+        /// </summary>
+        /// <param name="tone">The desired tone.</param>
         public void Play(Tone tone)
         {
             Play(tone.Frequency, tone.Duration);
         }
 
+        /// <summary>
+        /// Plays a tune (collection of tones).
+        /// </summary>
+        /// <param name="tune">The collection of tones to play.</param>
         public void Play(Tone[] tune)
         {
             for (var i = 0; i < tune.Length; i++)
@@ -33,6 +53,9 @@ namespace Robbo.Devices
             }
         }
 
+        /// <summary>
+        /// Disposes the Piezo's PWM channel.
+        /// </summary>
         public void Dispose()
         {
             pwm.Dispose();
